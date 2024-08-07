bef82cdd2c83683d41443c2b4e012c64db8c688e
Dino Danic <dino.danic@gmail.com>
2024-08-07 22:44:11 +0200

    notes

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -94,10 +94,10 @@ keymap("n", "<leader>lf", "<cmd>lua vim.lsp.buf.format{async = true}<cr>", opts)
 keymap("n", "<leader>sc", "<cmd>Telescope colorscheme<cr>", opts)
 keymap("n", "<leader>sr", "<cmd>Telescope oldfiles<cr>", opts)
 keymap("n", "<leader>sk", "<cmd>Telescope keymaps<cr>", opts)
-keymap("n", "<leader>st", "<cmd>Telescope help_tags<cr>", opts)        -- list available help tags
-keymap("n", "<leader>ss", "<cmd>Telescope grep_string<cr>", opts)      -- find string under cursor in current working directory
--- keymap("n", "<leader>sf", "<cmd>Telescope flutter commands<cr>", opts)  
-keymap("n", "<leader>sf", "<cmd>Telescope flutter fvm commands<cr>", opts)  -- with fvm
+keymap("n", "<leader>st", "<cmd>Telescope help_tags<cr>", opts)            -- list available help tags
+keymap("n", "<leader>ss", "<cmd>Telescope grep_string<cr>", opts)          -- find string under cursor in current working directory
+-- keymap("n", "<leader>sf", "<cmd>Telescope flutter commands<cr>", opts)
+keymap("n", "<leader>sf", "<cmd>Telescope flutter fvm commands<cr>", opts) -- with fvm
 
 -- register
 keymap("n", "<leader>\"", "<cmd>register<cr>", opts) -- find string under cursor in current working directory
